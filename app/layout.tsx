7cf2d3867582c4740488b00c9fd4d9c7c0bb46f4
Jeff Ray <jeffmlazo@yahoo.com>
2025-05-19 19:25:47 +0800

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import Navbar from "@/components/navbar/navbar";
+import "./globals.css";
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Book Your Luxury Room",
+  description:
+    "Book now with special offer just for you today. Online Booking Hotel",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.variable} antialiased`}>
+        <Navbar />
+        <main className="bg-gray-50 min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
