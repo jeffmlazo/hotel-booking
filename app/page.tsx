be188c33068b677987817f404256d6c6c65e7f7e
Jeff Ray <jeffmlazo@yahoo.com>
2025-05-20 00:23:40 +0800

    Add Navbar, Navlink, Card, Footer, Hero & Main components Add logo dark & hotel hero image section Add layout component Add initial Cards list

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Hero from "@/components/Hero";
+import Main from "@/components/Main";
+
 export default function Home() {
   return (
     <>
       <div className="home-page">
-        <h1>Home Page</h1>
+        <Hero />
+        <div className="mt-16">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold uppercase">Room & Rates</h1>
+            <p className="py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ipsam.</p>
+          </div>
+          <Main />
+        </div>
       </div>
     </>
   );
