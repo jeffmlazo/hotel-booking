e3a48fadb81381fa30094b0fba6ec0bf2518f38c
Jeff Ray <jeffmlazo@yahoo.com>
2025-06-05 01:49:07 +0800

    Remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("_http_common");
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.externals.push("_http_common");
+  //   }
+  //   return config;
+  // },
   typescript: {
     ignoreBuildErrors: true,
   },
