1090270b2c87d30c1c89671636b1565a0be9267f
Jeff Ray <jeffmlazo@yahoo.com>
2025-06-05 01:25:15 +0800

    Add netlify configurations & packages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("_http_common");
+    }
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
