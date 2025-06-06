88e48287758aaa9a179c830993401884861f217c
Jeff Ray <jeffmlazo@yahoo.com>
2025-06-06 20:54:09 +0800

    Remove raleway font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
+// import { Raleway } from "next/font/google";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 import "./globals.css";
 
-const raleway = Raleway({
-  variable: "--font-raleway",
-  subsets: ["latin"],
-});
+// const raleway = Raleway({
+//   variable: "--font-raleway",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Book Your Luxury Room",
@@ -27,7 +27,8 @@ export default async function RootLayout({
   return (
     <html lang="en">
         <SessionProvider session={session}>
-          <body className={`${raleway.variable} antialiased`}>
+          {/* <body className={`${raleway.variable} antialiased`}> */}
+          <body className="antialiased">
             <Navbar />
             <main className="bg-gray-50 min-h-screen">{children}</main>
             <Footer />
