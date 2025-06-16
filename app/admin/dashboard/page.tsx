73800bcd6f1588064319596fd87c1d31bbeebe97
Jeff Ray <jeffmlazo@yahoo.com>
2025-06-16 23:57:18 +0800

    Add dasboard cards for Total Revenue, Total Reservation & Total Customers Add reservation list for admin dashboard Add isPending conditions for PaymentButton component Add query for getDisableRoomById, getRevenueAndReserve, getTotalCustomers & getReservations

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,7 +1,24 @@
+import { Metadata } from "next";
+import DashboardCards from "@/components/admin/DashboardCards";
+import ReservationList from "@/components/admin/ReservationList";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Dashboard"
+};
+
 const DashboardPage = () => {
   return (
     <>
-        <div className="py-20 mt-10">Dashboard Page</div>
+        <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+          <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+          <Suspense fallback={<p>Loading Cards...</p>}>
+            <DashboardCards />
+          </Suspense>
+          <Suspense fallback={<p>Loading Reservation...</p>}>
+            <ReservationList />
+          </Suspense>
+        </div>
     </>
   );
 };
